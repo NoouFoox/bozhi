82ab3a35be52d85dec88c80ed38a20ca045d82e4
贺俊浩 <nougat_hx@qq.com>
2024-10-12 16:10:00 +0800

    feat: 支持多窗口

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,7 @@
 use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
+use uuid::Uuid;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 // 更改窗口背景颜色
 #[tauri::command]
 fn set_background_color(window: tauri::Window, color: [f64; 4]) {
@@ -32,37 +29,45 @@ fn set_pinned(window: tauri::Window, pinned: bool) -> Result<(), String> {
     let _ = window.set_always_on_top(pinned);
     Ok(())
 }
-
+#[tauri::command]
+fn add_new_window(app: tauri::AppHandle) -> Result<(), String> {
+    _add_new_window(app)
+}
+fn _add_new_window(app: tauri::AppHandle) -> Result<(), String> {
+    // 随机位置
+    let postion = (rand::random::<f64>() * 500.0, rand::random::<f64>() * 500.0);
+    let uuid = Uuid::new_v4().to_string();
+    let new_window = WebviewWindowBuilder::new(&app, uuid, WebviewUrl::default())
+        .title("")
+        .position(postion.0, postion.1)
+        .inner_size(400.0, 400.0);
+    #[cfg(target_os = "macos")]
+    {
+        let new_window =  new_window.title_bar_style(TitleBarStyle::Transparent);
+        let window = new_window.build().unwrap();
+        let default_color =[255.0, 244.0, 163.0, 1.0];
+        set_window_background_color(&window, default_color);
+    }
+    Ok(())
+}
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
-                .title("")
-                .inner_size(400.0, 400.0);
-
-            #[cfg(target_os = "macos")]
-            let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
-
-            let window = win_builder.build().unwrap();
-            // window.open_devtools();
-            #[cfg(target_os = "macos")]
-            {
-                let default_color = [255.0, 244.0, 163.0, 1.0];
-                set_window_background_color(&window, default_color);
-            }
-
+            let capp:tauri::AppHandle = app.handle().clone();
+            _add_new_window(capp).unwrap();
             Ok(())
         })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             set_background_color,
-            set_pinned
+            set_pinned,
+            add_new_window
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
 fn set_window_background_color(window: &tauri::webview::WebviewWindow, color: [f64; 4]) {
     #[cfg(target_os = "macos")]
     {
--- src/ui/components/bo-zhi.tsx
+++ src/ui/components/bo-zhi.tsx
@@ -2,12 +2,16 @@ import { invoke } from "@tauri-apps/api/core";
 import { useMemo, useState } from "react";
 import { contrastColor, hexToRgba } from "../../lib/utils";
 import "./bo-zhi.css";
+import { Plus } from "lucide-react";
 import ColorInput from "./color-input";
 import BoZhiPin from "./bo-zhi-pin";
 import BoZhiEdit from "./bo-zhi-edit";
 const defaultColor = "#fff4aa";
 export default function boZhi() {
   const [color, setColor] = useState(defaultColor);
+  const addWindow = async (): Promise<void> => {
+    await invoke("add_new_window", { });
+  }
   const changeColor = async (value: string): Promise<void> => {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     await invoke("set_background_color", { color: hexToRgba(value) });
@@ -20,6 +24,7 @@ export default function boZhi() {
         <BoZhiEdit theme={barFontColor === "#ffffff" ? "classic" : "dark"} />
       </div>
       <div className="bo-zhi-tool-bar" style={{ color: barFontColor }}>
+        <Plus onClick={addWindow}></Plus>
         <ColorInput
           color={color}
           onChange={changeColor}
